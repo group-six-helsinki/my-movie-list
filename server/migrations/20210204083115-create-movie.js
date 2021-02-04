d7a9a560da1bc36bb0c92b12779af8d094a7d932
Shulthan Daffa'ul Iman <shulthan.daffaul.iman@gmail.com>
2021-02-04 18:10:11 +0700

    register + hashing bcrypt selesai

--- server/migrations/20210204083115-create-movie.js
+++ server/migrations/20210204083115-create-movie.js
@@ -1,33 +1,39 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Movies', {
+    await queryInterface.createTable("Movies", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       status: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
       },
       UserID: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Movies');
-  }
+    await queryInterface.dropTable("Movies");
+  },
 };
--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,6 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+const { hash } = require("../helpers/bcrypt");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -11,31 +10,40 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasMany(models.Movie, { foreignKey: 'UserID' })
+      User.hasMany(models.Movie, { foreignKey: "UserID" });
     }
-  };
-  User.init({
-    email: {
-      type: DataTypes.STRING,
-      validate: {
-        isEmail: {
-          args: true,
-          msg: 'Please enter a valid email address'
-        }
-      }
+  }
+  User.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: "Please enter a valid email address",
+          },
+        },
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [6],
+            msg: "Please enter a 6 characters password",
+          },
+        },
+      },
     },
-    password: {
-      type: DataTypes.STRING,
-      validate: {
-        len: {
-          args: [6],
-          msg: 'Please enter a 6 characters password'
-        }
-      }
+    {
+      sequelize,
+      modelName: "User",
+      hooks: {
+        beforeCreate(user, opt) {
+          user.password = hash(user.password);
+        },
+      },
     }
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  );
   return User;
 };
